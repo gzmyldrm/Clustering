f9862db341a65fc1ae2b52d28e5203847a206528
Gregor Kasieczka <g.kasieczka@gmail.com>
2014-11-16 17:00:17 +0100

    Added run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,11 @@
+python program.py distloc
+python program.py distpop
+python program.py distpop2
+python program.py distgdp
+python program.py distgdp2
+python program.py distpopgdp
+python program.py distpopgdp2
+python program.py distpopratio
+python program.py distgdpratio
+python program.py distpopgdpratio
+python program.py distpopgdpratio2
